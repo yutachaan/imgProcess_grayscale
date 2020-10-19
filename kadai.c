2bc4bb578505fda3d80ca02f20d0c7911324e0ff
SmileyLocker <yutachan010907@gmail.com>
2020-10-19 11:14:19 +0900

    :recycle: 重複部分を削除

--- kadai.c
+++ kadai.c
@@ -1,258 +1,145 @@
 /* TODO:
-   - output関係の関数の動作をまとめる
    - 自動でファイル名を決めるようにする
    - mallocで配列を確保するようにする
-   - 画像サイズをファイルから取得する用にする
 */
 
 #include <stdio.h>
 
 typedef struct {
-  unsigned char red;
-  unsigned char green;
-  unsigned char blue;
+  unsigned char r;
+  unsigned char g;
+  unsigned char b;
 } RGB;
 
-void read_header(FILE *image);                        // ヘッダの読み飛ばし
-void output_red(RGB rgb[]);                           // カラー画像の赤だけを取り出し出力
-void output_green(RGB rgb[]);                         // カラー画像の緑だけを取り出し出力
-void output_blue(RGB rgb[]);                          // カラー画像の青だけを取り出し出力
-void output_mean(RGB rgb[]);                          // 赤・緑・青の平均を求め出力
-void output_Y(RGB rgb[]);                             // 輝度Yを求め出力
-unsigned char convert_to_gray(RGB rgb[], int mode);   // グレイスケールに変換
+void jump_header(FILE *img);
+void output_img(RGB rgb[], char filepath[]);
+unsigned char convert_to_gray(RGB rgb[], int mode);
 
 int main(int argc, char *argv[]) {
-  FILE *image;    // ファイルポインタ
+  FILE *img;      // 元画像
   RGB rgb[65536]; // RGB画像用の配列
 
   // コマンドライン引数の数が適切でない場合プログラムを終了
   if (argc < 2) {
-    printf("ファイルのパスを入力してください。\n");
+    printf("画像ファイルのパスを指定してください。\n");
     return -1;
   }
   else if (argc > 2) {
-    printf("指定できるファイルは1つです。\n");
+    printf("指定できる画像ファイルは1つです。\n");
     return -1;
   }
 
-  // 第一引数で指定された画像ファイルを開く
-  image = fopen(argv[1], "rb");
+  // 第一引数で指定された画像ファイルを読み込みモードで開く
+  img = fopen(argv[1], "rb");
 
   // ファイルを開けなかった場合プログラムを終了
-  if (image == NULL) {
+  if (img == NULL) {
     printf("ファイルが開けませんでした。\n");
     return -1;
   }
 
   // ヘッダの読み飛ばし
-  read_header(image);
+  jump_header(img);
 
   // 画像データの読み込み
-  fread(rgb, sizeof(RGB), 65536, image);
+  fread(rgb, sizeof(RGB), 65536, img);
 
   // 画像ファイルを閉じる
-  fclose(image);
+  fclose(img);
 
-  // カラー画像の赤だけを取り出し、出力
-  output_red(rgb);
+  // カラー画像から赤を抽出して出力
+  output_img(rgb, "images/red.pgm");
 
-  // カラー画像の緑だけを取り出し、出力
-  output_green(rgb);
+  // カラー画像から緑を抽出して出力
+  output_img(rgb, "images/green.pgm");
 
-  // カラー画像の青だけを取り出し、出力
-  output_blue(rgb);
+  // カラー画像から青を抽出して出力
+  output_img(rgb, "images/blue.pgm");
 
-  // 赤・緑。青の平均を求め、出力
-  output_mean(rgb);
+  // 赤・緑・青の平均値を求め、抽出
+  output_img(rgb, "images/mean.pgm");
 
-  // 輝度Yを求め、出力
-  output_Y(rgb);
+  // 輝度Yを求め、抽出
+  output_img(rgb, "images/y.pgm");
 
   return 0;
 }
 
-void read_header(FILE *image) {
+// ヘッダを読み飛ばす(img: 読み飛ばす画像のファイルポインタ)
+void jump_header(FILE *img) {
   char buf[256];       // ヘッダ読み込み時のバッファ
   int count_line = 3;  // コメントなしの場合のヘッダの行数
 
   // ヘッダの行数が0になるまで読み飛ばす
   while (count_line != 0) {
-    fgets(buf, sizeof(buf), image);
+    fgets(buf, sizeof(buf), img);       // 1行読み飛ばす
     if (buf[0] != '#') count_line -= 1; // 読み込んだ行がコメントではない場合countを減らす
   }
 }
 
-void output_red(RGB rgb[]) {
-  int i;
-  FILE *image_red;           // ファイルポインタ
-  unsigned char gray[65536]; // グレイスケール変換後のデータの保存
+// 画像をグレイスケールに変換し出力(rgb: RGB画像のデータ, filepath: 保存する画像のパス)
+void output_img(RGB rgb[], char filepath[]) {
+  int i;                     // イテレータ用
+  static int mode;           // 変換するモードを管理
+  FILE *img_proc;            // 変換後の画像
+  unsigned char gray[65536]; // グレイスケール変換後の画像データ
+
+  // モードを更新
+  mode++;
 
   // 書き込むファイルを開く
-  image_red = fopen("images/red.pgm", "wb");
+  img_proc = fopen(filepath, "wb");
 
-  // ファイルを開けなかった場合プログラムを終了
-  if (image_red == NULL) {
+  // ファイルを開なかった場合プログラムを終了
+  if (img_proc == NULL) {
     printf("ファイルが開けませんでした。\n");
     return;
   }
 
   // ヘッダを書き込む
-  fputs("P5\n256 256\n255\n", image_red);
+  fputs("P5\n256 256\n255\n", img_proc);
 
-  // グレイスケールに変換
+  // モードを指定して画素ごとにグレイスケールに変換
   for (i=0; i<65536; i++) {
-    gray[i] = convert_to_gray(&rgb[i], 0);
+    gray[i] = convert_to_gray(&rgb[i], mode);
   }
 
-  // 画像データを書き出す
-  fwrite(gray, sizeof(unsigned char), 65536, image_red);
+  // 画像データを書き込む
+  fwrite(gray, sizeof(unsigned char), 65536, img_proc);
 
   // 画像ファイルを閉じる
-  fclose(image_red);
+  fclose(img_proc);
 }
 
-void output_green(RGB rgb[]) {
-  int i;
-  FILE *image_green;         // ファイルポインタ
-  unsigned char gray[65536]; // グレイスケール変換後のデータの保存
-
-  // 書き込むファイルを開く
-  image_green = fopen("images/green.pgm", "wb");
-
-  // ファイルを開けなかった場合プログラムを終了
-  if (image_green == NULL) {
-    printf("ファイルが開けませんでした。\n");
-    return;
-  }
-
-  // ヘッダを書き込む
-  fputs("P5\n256 256\n255\n", image_green);
-
-  // グレイスケールに変換
-  for (i=0; i<65536; i++) {
-    gray[i] = convert_to_gray(&rgb[i], 1);
-  }
-
-  // 画像データを書き出す
-  fwrite(gray, sizeof(unsigned char), 65536, image_green);
-
-  // 画像ファイルを閉じる
-  fclose(image_green);
-}
-
-void output_blue(RGB rgb[]) {
-  int i;
-  FILE *image_blue;          // ファイルポインタ
-  unsigned char gray[65536]; // グレイスケール変換後のデータの保存
-
-  // 書き込むファイルを開く
-  image_blue = fopen("images/blue.pgm", "wb");
-
-  // ファイルを開けなかった場合プログラムを終了
-  if (image_blue == NULL) {
-    printf("ファイルが開ませんでした。\n");
-    return;
-  }
-
-  // ヘッダを書き込む
-  fputs("P5\n256 256\n255\n", image_blue);
-
-  // グレイスケールに変換
-  for (i=0; i<65536; i++) {
-    gray[i] = convert_to_gray(&rgb[i], 2);
-  }
-
-  // 画像データを書き出す
-  fwrite(gray, sizeof(unsigned char), 65536, image_blue);
-
-  // 画像ファイルを閉じる
-  fclose(image_blue);
-}
-
-void output_mean(RGB rgb[]) {
-  int i;
-  FILE *image_mean;          // ファイルポインタ
-  unsigned char gray[65536]; // グレイスケール変換後のデータの保存
-
-  // 書き込むファイルを開く
-  image_mean = fopen("images/mean.pgm", "wb");
-
-  // ファイルを開けなかった場合プログラムを終了
-  if (image_mean == NULL) {
-    printf("ファイルが開ませんでした。\n");
-    return;
-  }
-
-  // ヘッダを書き込む
-  fputs("P5\n256 256\n255\n", image_mean);
-
-  // グレイスケールに変換
-  for (i=0; i<65536; i++) {
-    gray[i] = convert_to_gray(&rgb[i], 3);
-  }
-
-  // 画像データを書き出す
-  fwrite(gray, sizeof(unsigned char), 65536, image_mean);
-
-  // 画像ファイルを閉じる
-  fclose(image_mean);
-}
-
-void output_Y(RGB rgb[]) {
-  int i;
-  FILE *image_Y;             // ファイルポインタ
-  unsigned char gray[65536]; // グレイスケール変換後のデータの保存
-
-  // 書き込むファイルを開く
-  image_Y = fopen("images/y.pgm", "wb");
-
-  // ファイルを開けなかった場合プログラムを終了
-  if (image_Y == NULL) {
-    printf("ファイルが開ませんでした。\n");
-    return;
-  }
-
-  // ヘッダを書き込む
-  fputs("P5\n256 256\n255\n", image_Y);
-
-  // グレイスケールに変換
-  for (i=0; i<65536; i++) {
-    gray[i] = convert_to_gray(&rgb[i], 4);
-  }
-
-  // 画像データを書き出す
-  fwrite(gray, sizeof(unsigned char), 65536, image_Y);
-
-  // 画像ファイルを閉じる
-  fclose(image_Y);
-}
-
+// 画素をグレイスケールに変換(rgb: 変換する画素, mode: 変換するモード)
 unsigned char convert_to_gray(RGB rgb[], int mode) {
-  unsigned char gray; // グレイスケール変換後の画素のデータ
+  unsigned char gray; // グレイスケール変換後の画素データ
 
   switch (mode) {
-    case 0:
-      // RGBの赤だけを抽出
-      gray = rgb->red;
-      break;
     case 1:
-      // RGBの緑だけを抽出
-      gray = rgb->green;
+      // 赤だけを抽出
+      gray = rgb->r;
       break;
     case 2:
-      // RGBの青だけを抽出
-      gray = rgb->blue;
+      // 緑だけを抽出
+      gray = rgb->g;
       break;
     case 3:
-      // RGBのそれぞれの値の平均値を求める
-      gray = (rgb->red + rgb->green + rgb->blue) / 3;
+      // 青だけを抽出
+      gray = rgb->b;
       break;
     case 4:
+      // 赤・緑・青の平均値を求める
+      gray = (rgb->r + rgb->g + rgb->b) / 3;
+      break;
+    case 5:
       // 輝度Yを求める
-      gray = 0.299 * rgb->red + 0.587 * rgb->green + 0.114 * rgb->blue;
+      gray = 0.299 * rgb->r + 0.587 * rgb->g + 0.114 * rgb->b;
+      break;
     default:
       break;
   }
 
+  // 画素データを返す
   return gray;
 }
