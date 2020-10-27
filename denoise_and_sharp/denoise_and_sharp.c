e721f15b21fd329ce0d28a3ea17b93f0a529c391
SmileyLocker <yutachan010907@gmail.com>
2020-10-27 22:19:35 +0900

    :tada: 第五回開始

---
+++ denoise_and_sharp/denoise_and_sharp.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_header(FILE *img, int *width, int *height);
+
+int main(int argc, char *argv[]) {
+  FILE *img;            // 元画像
+  int width, height;    // 画像の横幅, 縦幅, 最大階調値
+  unsigned char *gray;  // グレイスケール画像データ
+
+  // コマンドライン引数の数が適切でない場合プログラムを終了
+  if (argc != 2) exit(1);
+
+  // 第一引数で指定された画像ファイルを開く(開けなかった場合プログラムを終了)
+  if ((img = fopen(argv[1], "rb")) == NULL) {
+    printf("ファイルが開けませんでした。\n");
+    exit(1);
+  }
+
+  // ヘッダを読み取り、画像の横幅と縦幅を代入
+  read_header(img, &width, &height);
+
+  // 配列を動的に確保(確保できなかった場合プログラムを終了)
+  if ((gray = (unsigned char *)malloc(sizeof(unsigned char) * width * height)) == NULL) {
+    printf("メモリが確保できませんでした。\n");
+    exit(1);
+  }
+
+  // 画像データの読み込み
+  fread(gray, sizeof(unsigned char), width * height, img);
+
+  free(gray);
+
+  fclose(img);
+
+  return 0;
+}
+
+// ヘッダの情報を読み取る(img: 読み込む画像のファイルポインタ, width: 画像の横幅, height: 画像の縦幅)
+void read_header(FILE *img, int *width, int *height) {
+  int i;
+  char buf[256];
+
+  while (i < 3) {
+    fgets(buf, sizeof(buf), img);
+
+    // コメントの場合読み飛ばす
+    if (buf[0] == '#') continue;
+
+    // 画像の横幅と縦幅を読み取る
+    if (i == 1) sscanf(buf, "%d %d", width, height);
+
+    i++;
+  }
+}
