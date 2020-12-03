ffb869e296ad5e5f41d4be377f64c07a40982277
yutachaan <yutachan010907@gmail.com>
2020-12-03 20:40:03 +0900

    :sparkles: laplacian終了

--- edge_detection/edge_detection.c
+++ edge_detection/edge_detection.c
@@ -7,9 +7,9 @@ void sobel_filter(unsigned char gray[], int width, int height);
 void laplacian_filter(unsigned char gray[], int width, int height);
 
 int main(int argc, char *argv[]) {
-  FILE *img;              // 元画像
-  int width, height;      // 画像の横幅, 縦幅
-  unsigned char *gray;    // グレイスケール画像データ
+  FILE *img;           // 元画像
+  int width, height;   // 画像の横幅, 縦幅
+  unsigned char *gray; // グレイスケール画像データ
 
   // コマンドライン引数の数が適切でない場合プログラムを終了
   if (argc != 2) exit(1);
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 
   sobel_filter(gray, width, height);
 
-  // laplacian_filter(gray, width, height);
+  laplacian_filter(gray, width, height);
 
   free(gray);
 
@@ -75,7 +75,7 @@ void sobel_filter(unsigned char gray[], int width, int height) {
       neighbor[6] = gray[pos + width - 1] * (-1);
       neighbor[8] = gray[pos + width + 1];
 
-      // 水平方向のフィルタ適用後の近傍画素の合計値を新しい値とする
+      // 水平方向のフィルタ適用後の近傍画素の合計値を求める
       int sum_h = 0;
       for (int k = 0; k < 9; k++) sum_h += neighbor[k];
 
@@ -88,11 +88,11 @@ void sobel_filter(unsigned char gray[], int width, int height) {
       neighbor[7] = gray[pos + width] * 2;
       neighbor[8] = gray[pos + width + 1];
 
-      // 垂直方向のフィルタ適用後の近傍画素の合計値を新しい値とする
+      // 垂直方向のフィルタ適用後の近傍画素の合計値を求める
       int sum_v = 0;
       for (int k = 0; k < 9; k++) sum_v += neighbor[k];
 
-      // Sabelフィルタを適用
+      // Sabelフィルタを適用(255より大きい場合は255にする)
       if ((int)sqrt(pow(sum_h, 2) + pow(sum_v, 2)) > 255) sobel[pos] = 255;
       else sobel[pos] = (int)sqrt(pow(sum_h, 2) + pow(sum_v, 2));
     }
@@ -136,8 +136,54 @@ void laplacian_filter(unsigned char gray[], int width, int height) {
   FILE *img_lap;                     // Laplacianフィルタ適用後の画像ファイル
   unsigned char lap[width * height]; // Laplacianフィルタ適用後の画像データ
 
+  // 元画像にラプラシアンフィルタを適用
+  for (int i = 1; i < width - 1; i++) {
+    for (int j = 1; j < height - 1; j++) {
+      // 注目画素の座標を求める
+      int pos = j * width + i;
+
+      // ラプラシアンフィルタ適用後の近傍画素の値を求める
+      int neighbor[9];
+      neighbor[0] = neighbor[2] = neighbor[6] = neighbor[8] = 0;
+      neighbor[1] = gray[pos - width];
+      neighbor[3] = gray[pos - 1];
+      neighbor[4] = gray[pos] * (-4);
+      neighbor[5] = gray[pos + 1];
+      neighbor[7] = gray[pos + width];
+
+      // ラプラシアンフィルタ適用後の近傍画素の合計値を求める
+      int sum_lap = 0;
+      for (int k = 0; k < 9; k++) sum_lap += neighbor[k];
+
+      // 合計値の絶対値を新しい値とする(255より大きい場合は255にする)
+      if (abs(sum_lap) > 255) lap[pos] = 255;
+      else lap[pos] = abs(sum_lap);
+    }
+  }
+
+  // ミラーリング
+  for (int i = 0; i < width; i++) {
+    int pos = (height - 1) * width + i;
+    if (i == 0) {
+      lap[i] = lap[i + width + 1];
+      lap[pos] = lap[pos - width + 1];
+    }
+    else if (i == width - 1) {
+      lap[i] = lap[i + width - 1];
+      lap[pos] = lap[pos - width - 1];
+    }
+    else {
+      lap[i] = lap[i + width];
+      lap[pos] = lap[pos - width];
+    }
+  }
+  for (int j = 1; j < height - 1; j++) {
+    lap[j * width] = lap[j * width + 1];
+    lap[j * width + width - 1] = lap[j * width + width - 2];
+  }
+
   // 書き込むファイルを開く(開けなかった場合プログラムを終了)
-  if ((img_lap = fopen("lap_lenna.pgm", "wb")) == NULL) exit(1);
+  if ((img_lap = fopen("lap_airplane.pgm", "wb")) == NULL) exit(1);
 
   // ヘッダを書き込む
   fprintf(img_lap, "P5\n%d %d\n255\n", width, height);
