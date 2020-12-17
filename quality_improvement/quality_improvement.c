cede7bd55aa8bfbd6ef31fda3d9890d42dd6bd59
yutachaan <yutachan010907@gmail.com>
2020-12-17 20:49:23 +0900

    :poop: バグる

--- quality_improvement/quality_improvement.c
+++ quality_improvement/quality_improvement.c
@@ -8,6 +8,8 @@
 // 最小値を求めるマクロ
 #define MIN(X, Y) ((X) < (Y) ? (X) : (Y))
 
+#define MAX_DEPTH 255
+
 typedef struct {
   unsigned char r;
   unsigned char g;
@@ -20,17 +22,17 @@ typedef struct {
   double v;
 } HSV;
 
-void read_header(FILE *img, int *width, int *height, int *max_depth);
-void improve_quality(RGB rgb[], unsigned char gray[], int width, int height, int max_depth);
-void rgb2hsv(RGB rgb[], HSV hsv[], int max_depth);
-void hsv2rgb(HSV hsv[], RGB rgb[]);
+void read_header(FILE *img, int *width, int *height);
+void improve_quality(RGB rgb[], unsigned char gray[], int width, int height);
+void rgb2hsv(RGB rgb, HSV hsv[]);
+void hsv2rgb(HSV hsv, RGB rgb[]);
 
 int main(int argc, char *argv[]) {
-  FILE *img_color;              // 低画質RGB画像
-  FILE *img_high;               // グレイスケール画像
-  int width, height, max_depth; // 画像の横幅、縦幅、最大階調値
-  RGB *rgb;                     // RGB画像用
-  unsigned char *gray;          // グレイスケール画像データ
+  FILE *img_color;     // 低画質RGB画像
+  FILE *img_high;      // グレイスケール画像
+  RGB *rgb;            // RGB画像用
+  unsigned char *gray; // グレイスケール画像データ
+  int width, height;   // 画像の横幅、縦幅、最大階調値
 
   // コマンドライン引数の数が適切でない場合プログラムを終了
   if (argc != 3) exit(1);
@@ -38,7 +40,7 @@ int main(int argc, char *argv[]) {
   // 第一引数で指定された画像ファイル(低画質RGB画像)を開く(開けなかった場合プログラムを終了)
   if ((img_color = fopen(argv[1], "rb")) == NULL) exit(1);
 
-  read_header(img_color, &width, &height, &max_depth);
+  read_header(img_color, &width, &height);
 
   // RGB画像用の配列を動的に確保(確保できなかった場合プログラムを終了)
   if ((rgb = (RGB *)malloc(sizeof(RGB) * width * height)) == NULL) exit(1);
@@ -59,7 +61,7 @@ int main(int argc, char *argv[]) {
 
   fclose(img_high);
 
-  improve_quality(rgb, gray, width, height, max_depth);
+  improve_quality(rgb, gray, width, height);
 
   free(rgb);
 
@@ -68,8 +70,8 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-// ヘッダの情報を読み取る(img: 読み込む画像のファイルポインタ, width: 画像の横幅, height: 画像の縦幅, max_depth: 最大階調値)
-void read_header(FILE *img, int *width, int *height, int *max_depth) {
+// ヘッダの情報を読み取る(img: 読み込む画像のファイルポインタ, width: 画像の横幅, height: 画像の縦幅)
+void read_header(FILE *img, int *width, int *height) {
   char buf[256];
 
   int i;
@@ -82,43 +84,39 @@ void read_header(FILE *img, int *width, int *height, int *max_depth) {
     // 画像の横幅と縦幅を読み取る
     if (i == 1) sscanf(buf, "%d %d", width, height);
 
-    // 最大階調値を読み取る
-    if (i == 2) sscanf(buf, "%d", max_depth);
-
     i++;
   }
 }
 
-// HSVによる画質の改善(rgb: RGB画像のデータ, gray: グレイスケール画像のデータ, width: 画像の幅, height: 画像の高さ, max_depth: 最大階調値)
-void improve_quality(RGB rgb[], unsigned char gray[], int width, int height, int max_depth){
+// HSVによる画質の改善(rgb: RGB画像のデータ, gray: グレイスケール画像のデータ, width: 画像の幅, height: 画像の高さ)
+void improve_quality(RGB rgb[], unsigned char gray[], int width, int height){
   FILE *img_color_high;         // 高画質RGB画像
-  RGB rgb_high[width * height]; // 高画質RGB画像データ
   HSV hsv[width * height];      // HSV変換後の画像データ
 
-  for (int i = 0; i < width; i++) {
-    rgb2hsv(&rgb[i], &hsv[i], max_depth); // 低画質RGB画像のデータをHSV画像に変換
-    hsv[i].v = gray[i];                   // 輝度は高画質グレイスケール画像のものを利用
-    hsv2rgb(&hsv[i], &rgb_high[i]);       // HSV画像のデータをRGB画像に変換して高画質化
+  for (int i = 0; i < width * height; i++) {
+    rgb2hsv(rgb[i], &hsv[i]);               // 低画質RGB画像のデータをHSV画像に変換
+    hsv[i].v = gray[i] / (double)MAX_DEPTH; // 輝度は高画質グレイスケール画像のものを利用
+    hsv2rgb(hsv[i], &rgb[i]);               // HSV画像のデータをRGB画像に変換して高画質化
   }
 
   // 書き込むファイルを開く(開けなかった場合プログラムを終了)
   if ((img_color_high = fopen("color_high.ppm", "wb")) == NULL) exit(1);
 
   // ヘッダを書き込む
-  fprintf(img_color_high, "P6\n%d %d\n%d\n", width, height, max_depth);
+  fprintf(img_color_high, "P6\n%d %d\n%d\n", width, height, MAX_DEPTH);
 
   // 画像データを書き込む
-  fwrite(rgb_high, sizeof(RGB), width * height, img_color_high);
+  fwrite(rgb, sizeof(RGB), width * height, img_color_high);
 
   fclose(img_color_high);
 }
 
-// RGBをHSVに変換(rgb: RGB画像のデータ, hsv: HSV画像のデータ, max_depth: 最大階調値)
-void rgb2hsv(RGB rgb[], HSV hsv[], int max_depth) {
+// RGBをHSVに変換(rgb: RGB画像のデータ, hsv: HSV画像のデータ)
+void rgb2hsv(RGB rgb, HSV hsv[]) {
   // R, G, Bを0〜1の範囲に変換
-  double r = rgb->r / (double)max_depth;
-  double g = rgb->g / (double)max_depth;
-  double b = rgb->b / (double)max_depth;
+  double r = rgb.r / (double)MAX_DEPTH;
+  double g = rgb.g / (double)MAX_DEPTH;
+  double b = rgb.b / (double)MAX_DEPTH;
 
   // Max, Minを求める
   double max = MAX(r, MAX(g, b));
@@ -155,12 +153,55 @@ void rgb2hsv(RGB rgb[], HSV hsv[], int max_depth) {
 }
 
 // HSVをRGBに変換(hsv: HSV画像のデータ, rgb: RGB画像のデータ)
-void hsv2rgb(HSV hsv[], RGB rgb[]) {
-  // H'を求める
+void hsv2rgb(HSV hsv, RGB rgb[]) {
+  double h2, f, p, q, t, r, g, b;
 
-  // fを求める
-
-  // p, q, tを求める
+  // H', f, p, q, tを求める
+  h2 = ((int)hsv.h / 60) % 6;
+  f = (hsv.h / 60) - h2;
+  p = hsv.v * (1 - hsv.s);
+  q = hsv.v * (1 - hsv.s * f);
+  t = hsv.v * (1 - (1 - f) * hsv.s);
 
   // R, G, Bを求める
+  switch ((int)h2) {
+    case 0:
+      r = hsv.v;
+      g = t;
+      b = p;
+      break;
+    case 1:
+      r = q;
+      g = hsv.v;
+      b = p;
+      break;
+    case 2:
+      r = p;
+      g = hsv.v;
+      b = t;
+      break;
+    case 3:
+      r = p;
+      g = q;
+      b = hsv.v;
+      break;
+    case 4:
+      r = t;
+      g = p;
+      b = hsv.v;
+      break;
+    case 5:
+      r = hsv.v;
+      g = p;
+      b = q;
+      break;
+    default:
+      r = g = b = hsv.v;
+      break;
+  }
+
+  // 最大階調値倍して代入
+  rgb->r = (int)(r * MAX_DEPTH);
+  rgb->g = (int)(g * MAX_DEPTH);
+  rgb->b = (int)(b * MAX_DEPTH);
 }
