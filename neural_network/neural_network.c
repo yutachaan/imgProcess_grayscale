bc1a8cfadf82330d53dd4ac751d1025468ff3487
yutachaan <yutachan010907@gmail.com>
2021-01-28 14:08:41 +0900

    :sparkles: 課題2終わり

--- neural_network/neural_network.c
+++ neural_network/neural_network.c
@@ -5,12 +5,14 @@
 
 #define INPUTNO 2
 #define MAXINPUTNO 350
+#define HIDDENNO 2
 
 double f_step(double u);
 double f_sigmoid(double u);
 void read_data(char *fileloc, double coord[][INPUTNO], int *n);
 void save_data(char filename[], double coord[][INPUTNO], double output[], int n);
 void simple_perceptron(double coord[][INPUTNO], double output[], double weight[], int n);
+void multilayer_perceptron(double coord[][INPUTNO], double output[], double weight_h[][INPUTNO + 1], double weight_o[], int n);
 
 int main(int argc, char *argv[]) {
   int n1, n2, n3;                     // 行数
@@ -59,7 +61,7 @@ int main(int argc, char *argv[]) {
   save_data("out/data03_or.csv", coord3, output3, n3);
 
   // <----------- 課題1(AND分類器) ---------->
-  double weight_and[INPUTNO + 1] = {1, 1, 1.2};
+  double weight_and[INPUTNO + 1] = {1, 1, 1.3};
 
   // data01
   simple_perceptron(coord1, output1, weight_and, n1);
@@ -74,25 +76,75 @@ int main(int argc, char *argv[]) {
   save_data("out/data03_and.csv", coord3, output3, n3);
 
   // <----------- 課題2(XOR分類器) ---------->
+  double weight_xor_h[HIDDENNO][INPUTNO + 1] = {{1, 1, 0.5}, {1, 1, 1.3}};
+  double weight_xor_o[HIDDENNO + 1] = {1, -1, 0.5};
+
+  // data01
+  multilayer_perceptron(coord1, output1, weight_xor_h, weight_xor_o, n1);
+  save_data("out/data01_xor.csv", coord1, output1, n1);
+
+  // data02
+  multilayer_perceptron(coord2, output2, weight_xor_h, weight_xor_o, n2);
+  save_data("out/data02_xor.csv", coord2, output2, n2);
+
+  // data03
+  multilayer_perceptron(coord3, output3, weight_xor_h, weight_xor_o, n3);
+  save_data("out/data03_xor.csv", coord3, output3, n3);
 
   return 0;
 }
 
 // 単純パーセプトロン(coord: 座標, output: 出力保存用, weight: 重み, n: 行数)
 void simple_perceptron(double coord[][INPUTNO], double output[], double weight[], int n) {
-  double u;
+  double u; // 入力に重みを掛けて閾値を引いた値
 
   for (int i = 0; i < n; i++) {
     u = 0;
 
-    // 入力に重みを掛けて閾値を引く
+    // uを求める
     for (int j = 0; j < INPUTNO; j++) {
       u += coord[i][j] * weight[j];
     }
     u -= weight[INPUTNO];
 
-    // 活性化関数に渡して出力
+    // uを活性化関数に渡して出力
     output[i] = f_step(u);
+  }
+}
+
+// 多層パーセプトロン(coord: 座標, output: 出力保存用, weight_h: 隠れ層での重み, weight_o: 出力層での重み, n: 行数)
+void multilayer_perceptron(double coord[][INPUTNO], double output[], double weight_h[][INPUTNO + 1], double weight_o[], int n){
+  double u, o;             // 入力に重みを掛けて閾値を引いた値(u: 隠れ層, o: 出力層)
+  double hidden[HIDDENNO]; // それぞれの隠れ層でのuの値
+
+
+  for (int i = 0; i < n; i++) {
+    // 初期化
+    for (int j = 0; j < HIDDENNO; j++) hidden[j] = 0;
+
+    // 隠れ層の計算
+    for (int j = 0; j < HIDDENNO; j++) {
+      u = 0;
+
+      // uを求める
+      for (int k = 0; k < INPUTNO; k++) {
+        u += coord[i][k] * weight_h[j][k];
+      }
+      u -= weight_h[j][INPUTNO];
+
+      // uを活性化関数に渡して格納
+      hidden[j] = f_step(u);
+    }
+
+    // 出力層の計算
+    o = 0;
+    for (int j = 0; j < HIDDENNO; j++) {
+      o += hidden[j] * weight_o[j];
+    }
+    o -= weight_o[HIDDENNO];
+
+    // oを活性化関数に渡して出力
+    output[i] = f_step(o);
   }
 }
 
