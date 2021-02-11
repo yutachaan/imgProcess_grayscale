b5045d56676962d767748644d65804042133a673
yutachaan <yutachan010907@gmail.com>
2021-02-11 11:26:54 +0900

    :tada: 15回開始&データの読み込み・出力まで

---
+++ neural_network_2/neural_network_2.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#define INPUTNO 3
+#define MAXINPUTNO 8
+
+double f_sigmoid(double u);
+void read_data(char *fileloc, double input[][INPUTNO], int teacher[], int *n);
+void save_data(char filename[], double input[][INPUTNO], int teacher[], double output[], int n);
+
+int main(int argc, char *argv[]) {
+  int n;
+  double input[MAXINPUTNO][INPUTNO];
+  int teacher[MAXINPUTNO];
+  double output[MAXINPUTNO];
+
+  read_data(argv[1], input, teacher, &n);
+  for (int i = 0; i < n; i++) output[i] = 0;
+  save_data("out/data_after.csv", input, teacher, output, n);
+}
+
+// データ読み込み(fileloc: 読み込むファイル, input: input保存用, teacher: 教師データ保存用, n: 行数保存用)
+void read_data(char *fileloc, double input[][INPUTNO], int teacher[], int *n) {
+  FILE *data;
+  int mm, nn;
+  char buf[256];
+
+  if ((data = fopen(fileloc, "r")) == NULL) exit(1);
+
+  nn = 0;
+  while (fgets(buf, sizeof(buf), data) != NULL) {
+    mm = 0;
+    char *token = strtok(buf, " ");
+
+    while (token != NULL) {
+      if (mm < 3) input[nn][mm] = atof(token); // 最初の3つはinputに格納
+      else teacher[nn] = atoi(token);          // 最後の1つはteacherに格納
+
+      token = strtok(NULL, " ");
+      mm++;
+    }
+
+    nn++;
+  }
+
+  fclose(data);
+
+  *n = nn;
+}
+
+// データを保存(filename: 保存するファイル名, input: input, teacher: 教師データ, output: 学習結果, n: 行数)
+void save_data(char filename[], double input[][INPUTNO], int teacher[], double output[], int n) {
+  FILE *data;
+
+  if ((data = fopen(filename, "w")) == NULL) exit(1);
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < 3; j++) {
+      fprintf(data, "%d,", (int)input[i][j]);
+    }
+    fprintf(data, "%d,%.4f\n", teacher[i], output[i]);
+  }
+
+  fclose(data);
+}
+
+// シグモイド関数
+double f_sigmoid(double u) {
+  return 1.0 / (1.0 + exp(-u));
+}
