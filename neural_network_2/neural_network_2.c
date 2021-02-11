0a4023430bfcd21b2c0e0b8018225c3f1f79015c
yutachaan <yutachan010907@gmail.com>
2021-02-11 13:18:29 +0900

    :construction: 出力層の重みの更新まで

--- neural_network_2/neural_network_2.c
+++ neural_network_2/neural_network_2.c
@@ -9,11 +9,13 @@
 #define MAXINPUTNO 10
 #define HIDDENNO 3
 #define LIMIT 0.01
+#define ALPHA 0.1
 
 double f_sigmoid(double u);
 void read_data(char *fileloc, double input[][INPUTNO], int teacher[], int *n);
 void save_data(char filename[], double input[][INPUTNO], int teacher[], double output[], int n);
-double forward(double e[], double wh[][INPUTNO], double wo[], double hi[]);
+double forward(double e[], double wh[][INPUTNO + 1], double wo[], double hi[]);
+void olearn(int teacher, double wo[], double hi[], double output);
 
 int main(int argc, char *argv[]) {
   int n;                             // 行数
@@ -21,29 +23,33 @@ int main(int argc, char *argv[]) {
   int teacher[MAXINPUTNO];           // 教師データ
   double output[MAXINPUTNO];         // 学習結果
 
-  // データの読み込み
+  // <---------- データの読み込み ---------->
   read_data(argv[1], input, teacher, &n);
 
-  // 各層の重みを乱数で初期化
-  double wh[HIDDENNO][INPUTNO]; // 隠れ層
-  double wo[HIDDENNO];          // 出力層
+  // <---------- 各層の重みを乱数で初期化 ---------->
+  double wh[HIDDENNO][INPUTNO + 1]; // 隠れ層
+  double wo[HIDDENNO + 1];          // 出力層
 
   srand((unsigned int)time(NULL));
 
   for (int i = 0; i < HIDDENNO; i++) {
-    wo[i] = rand() / (double)RAND_MAX;
-    for (int j = 0; j < INPUTNO; j++) {
+    for (int j = 0; j <= INPUTNO; j++) {
       wh[i][j] = rand() / (double)RAND_MAX;
     }
   }
 
-  // 学習
+  for (int i = 0; i <= HIDDENNO; i++) {
+    wo[i] = rand() / (double)RAND_MAX;
+  }
+
+  // <---------- 学習 ---------->
   double hidden[HIDDENNO]; // 隠れ層での値
   double err = DBL_MAX;    // 誤差
   while (err > LIMIT) {
     err = 0;
     for (int i = 0; i < n; i++) {
       output[i] = forward(input[i], wh, wo, hidden); // 出力値を計算
+      olearn(teacher[i], wo, hidden, output[i]);     // 出力層の重みを更新
     }
   }
 
@@ -53,21 +59,34 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-// 出力値を計算(e: 1行文のインプット, wh: 隠れ層の重み, wo: 出力層の重み, hi: 隠れ層での値)
-double forward(double e[], double wh[][INPUTNO], double wo[], double hi[]) {
+// 出力値を計算(data: 1行文のインプット, wh: 隠れ層の重み, wo: 出力層の重み, hi: 隠れ層での値)
+double forward(double data[], double wh[][INPUTNO + 1], double wo[], double hi[]) {
   double u, o; // 入力に重みを掛けた値(u: 隠れ層, o: 出力層)
 
   // 隠れ層の値を求める
   for (int i = 0; i < HIDDENNO; i++) {
     u = 0;
-    for (int j = 0; j < INPUTNO; j++) u += e[j] * wh[i][j];
+    for (int j = 0; j < INPUTNO; j++) u += data[j] * wh[i][j];
+    u -= wh[i][INPUTNO];
     hi[i] = f_sigmoid(u);
   }
 
   // 出力層の値を求めて返却
   o = 0;
   for (int i = 0; i < HIDDENNO; i++) o += hi[i] * wo[i];
+  o -= wo[HIDDENNO];
   return f_sigmoid(o);
+}
+
+// 出力層の重みの更新(teacher: 教師データ, wo: 出力層の重み, hi: 隠れ層の値, output: 出力値)
+void olearn(int teacher, double wo[], double hi[], double output) {
+  // 重みを更新
+  for (int i = 0; i < HIDDENNO; i++) {
+    wo[i] += ALPHA * (teacher - output) * output * (1 - output) * hi[i];
+  }
+
+  // 閾値を更新
+  wo[HIDDENNO] += ALPHA * (teacher - output) * output * (1 - output) * (-1);
 }
 
 // データ読み込み(fileloc: 読み込むファイル, input: input保存用, teacher: 教師データ保存用, n: 行数保存用)
